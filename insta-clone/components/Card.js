5af9653a9e9278758fbe5300ca5868a0790dbaef
Geetanshu Garg <geetanshugarg39@gmail.com>
2023-01-31 15:15:19 +0530

    added card component

---
+++ insta-clone/components/Card.js
@@ -0,0 +1,9 @@
+const Card = () => {
+  return (
+    <div className = " border-box w-1/4 border border-gray-700 flex justify-center px-5 py-5">
+
+    </div>
+  )
+}
+
+export default Card
---
+++ insta-clone/src/pages/login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from "..//..//components/Card"
+const login = () => {
+  return (
+    <div className = "flex justify-center items-center align-middle" >
+      <Card>
+        <input type= "text"></input>
+      </Card>
+    </div>
+  )
+}
+
+export default login
